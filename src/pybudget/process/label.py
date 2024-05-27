9378903568ffc47e687afaea6884fa68d3c5542c
Wilson <wilson.fearn@gmail.com>
2024-05-26 17:20:00 -0700

    Got the label loop working for the category and amount models.

--- src/pybudget/process/label.py
+++ src/pybudget/process/label.py
@@ -9,7 +9,7 @@ import pandas as pd
 from scipy.sparse import hstack, vstack
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 from sklearn.svm import LinearSVC
 
 
@@ -19,14 +19,15 @@ PreparedTransaction = namedtuple('PreparedTransaction', 'transaction_string cate
 class LabellingAssistant:
     model_name_to_class = {
         'svm': LinearSVC,
-        'nn': MLPRegressor
+        'mlpr': MLPRegressor,
+        'mlpc': MLPClassifier
     }
 
     vectorizer_name_to_class = {
         'tfidf': TfidfVectorizer
     }
 
-    def __init__(self, category_model='svm', amount_model='nn', vectorizer = 'tfidf'):
+    def __init__(self, category_model='mlpc', amount_model='mlpr', vectorizer = 'tfidf'):
         self.category_model_trained = False
         self.amount_model_trained = False
         self.vectorizer_trained = False
@@ -34,18 +35,19 @@ class LabellingAssistant:
         # TODO: Interface with StorageManager outside of this class for these models
         if os.path.exists(category_model):
             with open(category_model, 'rb') as f:
-                self.category_model = pickle.load(f)
+                self.category_model, self.category_to_label = pickle.load(f)
                 self.category_model_trained = True
 
         else:
             self.category_model = LabellingAssistant.model_name_to_class[category_model]()
+            self.category_to_label = None
 
         if os.path.exists(amount_model):
             with open(amount_model, 'rb') as f:
                 self.amount_model = pickle.load(f)
                 self.amount_model_trained = True
         else:
-            self.amount_model = LabellingAssistant.model_name_to_class[amount_model]()
+            self.amount_model = LabellingAssistant.model_name_to_class[amount_model](hidden_layer_sizes=(100, 50))
 
         if os.path.exists(vectorizer):
             with open(vectorizer, 'rb') as f:
@@ -53,7 +55,7 @@ class LabellingAssistant:
                 self.vectorizer_trained = True
 
         else:
-            self.vectorizer = LabellingAssistant.vectorizer_name_to_class[vectorizer](analyzer='char', ngram_range=(1, 5))
+            self.vectorizer = LabellingAssistant.vectorizer_name_to_class[vectorizer](analyzer='char', ngram_range=(1, 2))
 
     def expand_prepared_transactions_into_training_data(
         self,
@@ -101,16 +103,12 @@ class LabellingAssistant:
         if self.category_model_trained and not retrain:
             return
 
-        print(len(transactions))
-
         training_transactions = list()
 
         for transaction in transactions.itertuples():
             prepared_transaction = self.prepare_transaction_for_featurization(transaction)
             for training_transaction, category_label, amount_label in self.expand_prepared_transactions_into_training_data(prepared_transaction):
                 training_transactions.append((training_transaction, category_label, amount_label))
-
-        print(len(training_transactions))
 
         train_and_validation, test = train_test_split(training_transactions, test_size=0.2)
         train, validation = train_test_split(train_and_validation, test_size=0.2)
@@ -119,18 +117,17 @@ class LabellingAssistant:
         train_data, train_category_labels, train_amount_labels = zip(*train)
         validation_data, validation_category_labels, validation_amount_labels = zip(*validation)
 
-        category_to_label = { category: i for i, category in enumerate(train_category_labels) }
+        self.category_to_label = { category: i for i, category in enumerate(set(train_category_labels)) }
 
         for l in validation_category_labels:
-            category_to_label.setdefault(l, len(category_to_label))
+            self.category_to_label.setdefault(l, len(self.category_to_label))
 
         for l in test_category_labels:
-            category_to_label.setdefault(l, len(category_to_label))
-                
+            self.category_to_label.setdefault(l, len(self.category_to_label))
 
-        train_Y = np.array([category_to_label[category] for category in train_category_labels])
-        test_Y = np.array([category_to_label[category] for category in test_category_labels])
-        validation_Y = np.array([category_to_label[category] for category in validation_category_labels])
+        train_Y = np.array([self.category_to_label[category] for category in train_category_labels])
+        test_Y = np.array([self.category_to_label[category] for category in test_category_labels])
+        validation_Y = np.array([self.category_to_label[category] for category in validation_category_labels])
 
         self.train_vectorizer(train_data)
 
@@ -140,10 +137,46 @@ class LabellingAssistant:
 
         self.category_model.fit(train_X, train_Y)
 
-        print(self.category_model.score(validation_X, validation_Y))
+        print(f'Category Model Score: {self.category_model.score(validation_X, validation_Y)}')
     
-    def train_amount_model(self, transaction: pd.DataFrame) -> None:
-        raise NotImplementedError
+    def train_amount_model(self, transactions: pd.DataFrame, retrain: bool = False) -> None:
+        if self.amount_model_trained and not retrain:
+            return
+
+        training_transactions = list()
+
+        for transaction in transactions.itertuples():
+            prepared_transaction = self.prepare_transaction_for_featurization(transaction)
+            for training_transaction, category_label, amount_label in self.expand_prepared_transactions_into_training_data(prepared_transaction):
+                training_transactions.append((training_transaction, category_label, amount_label))
+
+        train_and_validation, test = train_test_split(training_transactions, test_size=0.2)
+        train, validation = train_test_split(train_and_validation, test_size=0.2)
+
+        test_data, test_category_labels, test_amount_labels = zip(*test)
+        train_data, train_category_labels, train_amount_labels = zip(*train)
+        validation_data, validation_category_labels, validation_amount_labels = zip(*validation)
+
+        for i in range(len(test_data)):
+            test_data[i].categories.append(test_category_labels[i])
+
+        for i in range(len(train_data)):
+            train_data[i].categories.append(train_category_labels[i])
+
+        for i in range(len(validation_data)):
+            validation_data[i].categories.append(validation_category_labels[i])
+
+        train_Y = np.array(train_amount_labels)
+        test_Y = np.array(test_amount_labels)
+        validation_Y = np.array(validation_amount_labels)
+
+        train_X = vstack(np.array([ self.featurize_prepared_transaction(d) for d in train_data ]))
+        test_X = vstack(np.array([ self.featurize_prepared_transaction(d) for d in test_data ]))
+        validation_X = vstack(np.array([ self.featurize_prepared_transaction(d) for d in validation_data ]))
+
+        self.amount_model.fit(train_X, train_Y)
+
+        print(f'Amount Model Score: {self.amount_model.score(validation_X, validation_Y)}')
     
     def train_vectorizer(self, training_data: List[Tuple[str, List[str], List[float], float]], retrain: bool = False) -> None:
         if self.vectorizer_trained and not retrain:
@@ -163,7 +196,7 @@ class LabellingAssistant:
         category_string = ' '.join(prepared_transaction.categories)
         X_vector = self.vectorizer.transform([f'{prepared_transaction.transaction_string} {category_string}'])
 
-        # We only expect there to be a maximum number of 5 categories per transaction -- could change
+        # NOTE: We only expect there to be a maximum number of 5 categories per transaction -- could change
         amount_vector = np.zeros(5)
         for i, amount in enumerate(prepared_transaction.amounts):
             amount_vector[i] = amount
@@ -178,20 +211,21 @@ class LabellingAssistant:
     def prepare_transaction_for_featurization(self, transaction: Tuple) -> Tuple[str, List[str], List[float], float]:
 
         amounts = transaction.amount.split(',')
-        categories = transaction.category.split(',')
+        categories = [ c for c in transaction.category.split(',') if c != 'TO_LABEL' ]
 
         date, description, institution = transaction.date, transaction.description, transaction.institution
         initial_string = f'{date} {description} {institution}'
 
-        total_amount = sum([float(a) for a in amounts])
+        float_amounts = [ float(a) for a in amounts ]
+        total_amount = sum(float_amounts)
 
-        return PreparedTransaction(initial_string, categories, amounts, total_amount)
+        return PreparedTransaction(initial_string, categories, float_amounts, total_amount)
 
     def label_transactions(self, transactions: pd.DataFrame, labels: List[str]) -> pd.DataFrame:
 
         transactions = transactions.copy()
 
-        label_to_category = { i: label for i, label in enumerate(labels) }
+        label_to_category = { label: category for category, label in self.category_to_label.items() }
 
         while 'TO_LABEL' in transactions['category'].unique():
 
@@ -201,45 +235,49 @@ class LabellingAssistant:
             for transaction in sampled_transactions.itertuples():
 
                 prepared_transaction = self.prepare_transaction_for_featurization(transaction)
+                prepared_transaction.amounts.clear()
 
                 while True:
 
                     featurized_transaction = self.featurize_prepared_transaction(prepared_transaction)
 
-                    # predicted_category = label_to_category[self.category_model.predict(featurized_transaction)]
-                    predicted_category = label_to_category[0]
-                    confirmed_category = self.confirm_predicted_category(transaction, predicted_category, labels)
-                    prepared_transaction.categories.append(confirmed_category)
+                    model_prediction = self.category_model.predict(featurized_transaction)
+                    predicted_index = model_prediction[0]
+                    predicted_category = label_to_category[predicted_index]
+                    confirmed_category = self.confirm_predicted_category(prepared_transaction, predicted_category, labels)
 
-                    featurized_transaction = self.featurize_prepared_transaction(prepared_transaction)
-                    # predicted_amount = self.amount_model.predict(featurized_transaction)
-                    predicted_amount = 10.0
-                    confirmed_amount = self.confirm_predicted_amount(transaction, confirmed_category, predicted_amount)
-                    prepared_transaction.amounts.append(confirmed_amount)
-
-                    print('Prepared Transaction:', prepared_transaction)
 
                     if confirmed_category == 'NONE':
                         break
+
+                    prepared_transaction.categories.append(confirmed_category)
+
+
+                    featurized_transaction = self.featurize_prepared_transaction(prepared_transaction)
+                    predicted_amount = self.amount_model.predict(featurized_transaction)
+                    confirmed_amount = self.confirm_predicted_amount(predicted_amount)
+                    prepared_transaction.amounts.append(confirmed_amount)
+                    print('\n\n')
+
 
 
         return transactions
 
     def confirm_predicted_category(
         self,
-        transaction: pd.Series,
+        transaction: Tuple[str, List[str], List[float], float],
         predicted_category: str,
         labels: List[str]
     ) -> str:
 
-        date, description, amount = transaction.date, transaction.description, transaction.amount
+        string, categories, amounts, total_amount = transaction.transaction_string, transaction.categories, transaction.amounts, transaction.total_amount
         confirmed_category = predicted_category
 
-        print('Date:', date)
-        print('Description:', description)
-        print('Amount:', amount)
+        print('Transaction:', string)
+        print('Total Amount:', total_amount)
+        print('Categories So Far:', categories)
+        print('Amounts So Far:', amounts)
         confirmation = input(f'Does {predicted_category} match this transaction? ')
-        print()
 
         if confirmation != 'y':
             num_to_label = { i: label for i, label in enumerate(labels) }
@@ -247,31 +285,20 @@ class LabellingAssistant:
             for i, label in num_to_label.items():
                 print(i, ':', label)
 
-            num = int(input('Which label matches this transaction? '))
+            num = int(input('\tWhich label matches this transaction? '))
             confirmed_category = num_to_label[num]
 
         return confirmed_category
 
-    def confirm_predicted_amount(
-        self,
-        transaction: pd.Series,
-        confirmed_category: str,
-        predicted_amount: float
-    ) -> float:
+    def confirm_predicted_amount(self, predicted_amount: float) -> float:
 
-        confirmed_amount = predicted_amount
+        confirmed_amount = predicted_amount[0]
 
-        date, description, amount = transaction.date, transaction.description, transaction.amount
-
-        print('Date:', date)
-        print('Description:', description)
-        print('Amount:', amount)
-        print('Predicted Category:', confirmed_category)
-        confirmation = input(f'Does {predicted_amount} match the predicted category for this total amount? ')
+        confirmation = input(f'Does {confirmed_amount} seem about right for this category? ')
         print()
 
         if confirmation != 'y':
-            confirmed_amount = float(input('What amount matches this category? '))
+            confirmed_amount = float(input('\tWhat amount matches this category? '))
 
         return confirmed_amount
 
