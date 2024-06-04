b263c9f4abe83a91ca4f9a86e9c864cb8f9f41c4
Wilson <wilson.fearn@gmail.com>
2024-06-03 19:34:06 -0700

    Added support for adding new categories

--- src/pybudget/account/manage.py
+++ src/pybudget/account/manage.py
@@ -6,5 +6,22 @@ def set_budget(budget: Dict[str, int]) -> Dict[str, int]:
         new_amount = input(f'What amount for {category} (current: {budget_amount})? ')
         budget[category] = int(new_amount) if new_amount else budget_amount
 
+    print()
+
+    additional_category = 'category'
+
+    while True:
+        if not additional_category: break
+
+        additional_category = input('What other category would you like to add? ')
+
+        if additional_category in budget:
+            print(f'\t{additional_category} already accounted for.')
+        elif additional_category and additional_category not in budget:
+            print()
+            amount = int(input(f'How much would you like to allocate for {additional_category}? '))
+            budget[additional_category] = amount
+
+    print()
     return budget
 
